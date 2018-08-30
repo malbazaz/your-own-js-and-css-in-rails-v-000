e7cd3107f6069af7731b7b5addea9775cc219c18
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-30 01:55:33 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -2,9 +2,5 @@ function hideWhenClicked(){
 // hides the element that was clicked
 // bind hide_this
 let x = document.getElementById('hide_this')
-if (x.display === "none") {
-    x.display = "block";
-} else {
     x.display = "none";
-}
 }
