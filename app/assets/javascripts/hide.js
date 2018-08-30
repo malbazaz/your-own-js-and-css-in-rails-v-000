edaac145f574e641a6443ac9a5ff263ac2eb9d0f
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-30 02:24:10 +0000

    Done.

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,10 +1,14 @@
 function hideWhenClicked(){
 // hides the element that was clicked
 // bind hide_this
-let x = document.getElementById('hide_this')
-if (x.style.display === "none") {
-    x.style.display = "block";
-} else {
-    x.style.display = "none";
+
+const x = document.getElementById('hide_this')
+x.addEventListener("click",function(){
+  x.style.display = "none";
+})
+
 }
-}
+
+$(document).ready(function() {
+  hideWhenClicked();
+});
