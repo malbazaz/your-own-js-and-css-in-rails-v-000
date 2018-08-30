89298c2302ca0209808e63778fc940e729d110ac
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-30 01:54:34 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -2,9 +2,9 @@ function hideWhenClicked(){
 // hides the element that was clicked
 // bind hide_this
 let x = document.getElementById('hide_this')
-if (x.style.display === "none") {
-    x.style.display = "block";
+if (x.display === "none") {
+    x.display = "block";
 } else {
-    x.style.display = "none";
+    x.display = "none";
 }
 }
