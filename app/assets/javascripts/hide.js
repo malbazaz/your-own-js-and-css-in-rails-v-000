0ed936f736dc7492f9e432ed49783def8fa287bd
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-30 02:15:47 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,14 +1,12 @@
 function hideWhenClicked(){
 // hides the element that was clicked
 // bind hide_this
-document.getElementById("myBtn").addEventListener("click", function(){
-    document.getElementById("demo").innerHTML = "Hello World";
-});
+
 const x = document.getElementById('hide_this')
 x.addEventListener("click",function(){
   x.display = "none";
 })
-    
+
 }
 
 $(document).ready(function() {
